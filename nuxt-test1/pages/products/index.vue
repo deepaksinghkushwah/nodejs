c52b1385782d3fc83aa1854a060a24e6878f581d
Deepak Singh Kushwah <deepaksinghkushwah@gmail.com>
2021-05-04 15:49:06 +0530

    routers and pages

---
+++ nuxt-test1/pages/products/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <nuxt-link to="/products/1">ID: 1</nuxt-link><br>
+        <nuxt-link to="/products/2">ID: 2</nuxt-link>
+    </div>
+</template>
---
+++ nuxt-test1/pages/users/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Hello Users</h1>
+        <input type="text" v-model="userID"/>
+        <button @click="sendUser">Click Here</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return  {
+            userID: 0
+        }
+    },
+    methods: {
+        sendUser(){
+            this.$router.push('/users/' + this.userID);
+        }
+    }
+}
+</script>
