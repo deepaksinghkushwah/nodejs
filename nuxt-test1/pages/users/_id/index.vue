46b614519741d61ef9d732824a13724f73b8c062
Deepak Singh Kushwah <deepaksinghkushwah@gmail.com>
2021-05-04 15:54:21 +0530

    validation

--- nuxt-test1/pages/users/_id/index.vue
+++ nuxt-test1/pages/users/_id/index.vue
@@ -1,3 +1,11 @@
 <template>
     <h1>Single user with id: {{ $route.params.id }}</h1>
 </template>
+<script>
+export default {
+    validate(data){
+        //return data.params.id == 1;
+        return /^\d+$/.test(data.params.id);
+    }
+}
+</script>
